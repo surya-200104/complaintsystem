df9a4968c21caa296ace1075b97c231aee0021d0
suryateja <suryatejanalamala940@gmail.com>
2026-04-09 16:13:03 +0530

    fix reset

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(200))
+    password = db.Column(db.String(500))
     phone = db.Column(db.String(20))
     is_admin = db.Column(db.Boolean, default=False)
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
